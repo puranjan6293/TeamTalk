ab263c08dc249a6cbf13cb444260bf5c6551973d
puranjan <mallik.puranjan@gmail.com>
2023-01-22 20:21:28 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/teamtalk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teamtalk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:velocity_x/velocity_x.dart';
+
+class Constants {
+  static String appId = "1:264169646038:web:86f780e3094c28c6346677";
+  static String apiKey = "AIzaSyB3RDO-YMmPItBVT4Bi-geWWp4J3OYQBOk";
+  static String messagingSenderId = "264169646038";
+  static String projectId = "teamtalk-c82a7";
+  final primaryColor = Vx.green400;
+}
